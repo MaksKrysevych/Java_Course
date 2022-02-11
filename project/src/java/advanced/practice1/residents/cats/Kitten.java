f8a6a38b6250086e8ab8a36350398f5ae117d7cd
MaksKrysevych <91612087+MaksKrysevych@users.noreply.github.com>
2022-02-11 17:57:27 +0200

    Add files via upload

---
+++ project/src/java/advanced/practice1/residents/cats/Kitten.java
@@ -0,0 +1,13 @@
+package advanced.practice1.residents.cats;
+
+public class Kitten extends Cat {
+
+    public Kitten(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Kitten " + name;
+    }
+}
---
+++ project/src/java/advanced/task1/Main.java
@@ -0,0 +1,26 @@
+package advanced.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayImpl array = new ArrayImpl();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+        System.out.println("All elements: " + array.toString());
+        System.out.println("Size: " + array.size());
+        System.out.println("Index of 4 element: " + array.indexOf(4));
+        System.out.println("Get element with index 4: " + array.get(4));
+        System.out.println("Delete element with index 1:");
+        array.remove(1);
+        System.out.println(array.toString());
+        System.out.println("Add new element 7 for 3 position");
+        array.set(2, 4);
+        System.out.println(array.toString());
+        System.out.println("Delete all elements:");
+        array.clear();
+        System.out.println(array.toString());
+
+    }
+}
